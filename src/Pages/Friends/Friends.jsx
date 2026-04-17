b9901233a5322217029477d2f06ae5759a353322
tanjimahsankayes@gmail.com <tanjimahsankayes@gmail.com>
2026-04-17 15:05:21 +0600

    tablet responsive

--- src/Pages/Friends/Friends.jsx
+++ src/Pages/Friends/Friends.jsx
@@ -14,7 +14,7 @@ const Friends = () => {
     <div className="mt-12 w-8/12 mx-auto">
       <h1 className="text-3xl font-bold mb-8">Your Friends</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {friends.map((friend, index) => (
           <FriendCard key={index} friends={friend} />
         ))}
